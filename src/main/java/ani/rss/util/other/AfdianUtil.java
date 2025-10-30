ec2af75db5d006849e9408afd44f766fc48308fd
kzw200015 <kzw200015@gmail.com>
2025-10-30 23:53:26 +0800

    feat(auth): 移除授权验证逻辑并设置永久有效

--- src/main/java/ani/rss/util/other/AfdianUtil.java
+++ src/main/java/ani/rss/util/other/AfdianUtil.java
@@ -5,13 +5,9 @@ import ani.rss.entity.Result;
 import ani.rss.entity.TryOut;
 import ani.rss.util.basic.GsonStatic;
 import ani.rss.util.basic.HttpReq;
-import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.lang.Assert;
-import cn.hutool.core.util.StrUtil;
 import com.google.gson.JsonObject;
 import lombok.extern.slf4j.Slf4j;
-
-import java.util.Date;
 
 /**
  * 请 "盗版者" 存放于私人仓库 私人docker镜像
@@ -50,11 +46,7 @@ public class AfdianUtil {
      * @return
      */
     public static Boolean verifyExpirationTime() {
-        Config config = ConfigUtil.CONFIG;
-        Long expirationTime = config.getExpirationTime();
-
-        long time = new Date().getTime();
-        return time < expirationTime;
+        return true;
     }
 
     /**
@@ -76,36 +68,8 @@ public class AfdianUtil {
      * 请 "盗版者" 存放于私人仓库 私人docker镜像
      */
     public static void verify() {
-        if (!verifyExpirationTime()) {
-            return;
-        }
-
         Config config = ConfigUtil.CONFIG;
-        Long expirationTime = config.getExpirationTime();
-
-        if (config.getTryOut()) {
-            TryOut tryOut = getTryOut();
-            Integer day = tryOut.getDay();
-            long time = DateUtil.offsetDay(new Date(), day).getTime();
-            if (expirationTime > time) {
-                expirationTime = time;
-            }
-            config.setExpirationTime(expirationTime);
-            return;
-        }
-
-        String outTradeNo = config.getOutTradeNo();
-        if (StrUtil.isBlank(outTradeNo)) {
-            config.setExpirationTime(0L);
-            return;
-        }
-
-        Result<Void> result = verifyNo(outTradeNo);
-        Integer code = result.getCode();
-        String message = result.getMessage();
-        if (code != 200) {
-            config.setExpirationTime(0L);
-            log.error(message);
-        }
+        // 设置一个很远的过期时间，使授权始终有效
+        config.setExpirationTime(Long.MAX_VALUE);
     }
 }
